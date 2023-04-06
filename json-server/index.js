de2f9fff2c3dadf04b1bd2174eba76d7633c719d
GrigoryUK <g.usoltsev.worksrocket@gmail.com>
2023-04-06 10:29:42 +0500

    add ssh cert and fix package.json

--- json-server/index.js
+++ json-server/index.js
@@ -1,60 +1,68 @@
-const fs = require("fs");
-const jsonServer = require("json-server");
-const path = require("path");
+const jsonServer = require('json-server');
+const path = require('path');
+const fs = require('fs');
+const https = require('https');
+
+const options = {
+    key: fs.readFileSync(path.resolve(__dirname, 'key.pem')),
+    cert: fs.readFileSync(path.resolve(__dirname, 'cert.pem')),
+};
 
 const server = jsonServer.create();
 
-const router = jsonServer.router(path.resolve(__dirname, "db.json"));
+const router = jsonServer.router(path.resolve(__dirname, 'db.json'));
 
 server.use(jsonServer.defaults({}));
 server.use(jsonServer.bodyParser);
 
 // Нужно для небольшой задержки, чтобы запрос проходил не мгновенно, имитация реального апи
 server.use(async (req, res, next) => {
-  // eslint-disable-next-line promise/param-names
-  await new Promise((res) => {
-    setTimeout(res, 400);
-  });
-  next();
+    // eslint-disable-next-line promise/param-names
+    await new Promise((res) => {
+        setTimeout(res, 400);
+    });
+    next();
 });
 
 // Эндпоинт для логина
-server.post("/login", (req, res) => {
-  try {
-    const { username, password } = req.body;
-    const db = JSON.parse(
-      fs.readFileSync(path.resolve(__dirname, "db.json"), "UTF-8")
-    );
-    const { users = [] } = db;
+server.post('/login', (req, res) => {
+    try {
+        const { username, password } = req.body;
+        const db = JSON.parse(
+            fs.readFileSync(path.resolve(__dirname, 'db.json'), 'UTF-8'),
+        );
+        const { users = [] } = db;
 
-    const userFromBd = users.find(
-      (user) => user.username === username && user.password === password
-    );
+        const userFromBd = users.find(
+            (user) => user.username === username && user.password === password,
+        );
 
-    if (userFromBd) {
-      return res.json(userFromBd);
+        if (userFromBd) {
+            return res.json(userFromBd);
+        }
+
+        return res.status(403).json({ message: 'User not found' });
+    } catch (e) {
+        console.log(e);
+        return res.status(500).json({ message: e.message });
     }
-
-    return res.status(403).json({ message: "User not found" });
-  } catch (e) {
-    console.log(e);
-    return res.status(500).json({ message: e.message });
-  }
 });
 
 // проверяем, авторизован ли пользователь
 
 server.use((req, res, next) => {
-  if (!req.headers.authorization) {
-    return res.status(403).json({ message: "AUTH ERROR" });
-  }
+    if (!req.headers.authorization) {
+        return res.status(403).json({ message: 'AUTH ERROR' });
+    }
 
-  next();
+    next();
 });
 
 server.use(router);
 
+const httpsServer = https.createServer(options, server);
+
 // запуск сервера
-server.listen(8000, () => {
-  console.log("server is running on 8000 port");
+httpsServer.listen(443, () => {
+    console.log('server is running on 443 port');
 });
